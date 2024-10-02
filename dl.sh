7f788c79df0db3a89c0a413a5284221d17be4d1b
Don Ferris <109878120+don-ferris@users.noreply.github.com>
2024-10-02 11:52:26 -0700

    Update dl.sh

--- dl.sh
+++ dl.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget -c https://lax4-4.download.real-debrid.com/d/R3LNFXUOEBCGG40/Stranger%20Than%20Fiction%202006%20%281080p%20x265%2010bit%20Tigole%29.mkv
+wget -c https://lax3-4.download.real-debrid.com/d/GTPE4VNE7RSVS40/A.River.Runs.Through.It.1992.2160p.UHD.BluRay.x265.10bit.HDR.DTS-HD.MA.5.1-RARBG.mkv
 
 #   4K|2160|UHD 10bit|10-bit|hdr blu|bd atmos|dts-x
