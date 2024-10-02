519698f33e5e739deb68ea78078c46a90477d2c8
Don Ferris <109878120+don-ferris@users.noreply.github.com>
2024-10-02 11:47:45 -0700

    Create dl.sh

---
+++ dl.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget -c https://lax4-4.download.real-debrid.com/d/R3LNFXUOEBCGG40/Stranger%20Than%20Fiction%202006%20%281080p%20x265%2010bit%20Tigole%29.mkv
+
+#   4K|2160|UHD 10bit|10-bit|hdr blu|bd atmos|dts-x
