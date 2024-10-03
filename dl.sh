d97f358a6e5bfe64d8b145c7cbdd66509fe1177d
Don Ferris <109878120+don-ferris@users.noreply.github.com>
2024-10-03 10:32:44 -0700

    Update dl.sh

--- dl.sh
+++ dl.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget -c https://lax1-4.download.real-debrid.com/d/5EPID22S4HJT288/Fight.Club.1999.2160p.x265.10bit.DTS-HD.MA.5.1%5BTheUpscaler%5D.mkv
+wget -c https://lax5-4.download.real-debrid.com/d/75TNKRNTJTYFW97/Love.Actually.2003.2160p.4K.WEB.x265.10bit.AAC5.1-%5BYTS.MX%5D.mkv
 
 
 #   4K|2160|UHD 10bit|10-bit|hdr blu|bd atmos|dts-x
