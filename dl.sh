9a65b4c357fa5537a2e7cbfbc3c3238eb570a58c
Don Ferris <109878120+don-ferris@users.noreply.github.com>
2024-10-03 13:39:21 -0700

    Update dl.sh

--- dl.sh
+++ dl.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget -c https://lax5-4.download.real-debrid.com/d/75TNKRNTJTYFW97/Love.Actually.2003.2160p.4K.WEB.x265.10bit.AAC5.1-%5BYTS.MX%5D.mkv
+wget -c https://lax2-4.download.real-debrid.com/d/WGQO4HUL2AU6274/The.Dark.Knight.2008.2160p.UHD.BluRay.x265.10bit.HDR.DTS-HD.MA.5.1-RARBG.mkv
 
 
 #   4K|2160|UHD 10bit|10-bit|hdr blu|bd atmos|dts-x
