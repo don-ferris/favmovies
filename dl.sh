b8f31a623cabbfcba23a279a181b64ae8290a16a
Don Ferris <109878120+don-ferris@users.noreply.github.com>
2024-10-03 15:35:33 -0700

    Update dl.sh

--- dl.sh
+++ dl.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget -c https://lax2-4.download.real-debrid.com/d/WGQO4HUL2AU6274/The.Dark.Knight.2008.2160p.UHD.BluRay.x265.10bit.HDR.DTS-HD.MA.5.1-RARBG.mkv
+wget -c https://lax5-4.download.real-debrid.com/d/GJCJXYZSWDGOQ93/I%20Robot.2004.4K.HDR.2160p.WEBDL%20Ita%20Eng%20x265-NAHOM.mkv
 
 
 #   4K|2160|UHD 10bit|10-bit|hdr blu|bd atmos|dts-x
