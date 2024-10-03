39a988099ba232f850f795275c78a43bb66716bc
Don Ferris <109878120+don-ferris@users.noreply.github.com>
2024-10-03 09:22:04 -0700

    Update dl.sh

--- dl.sh
+++ dl.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set download=https://lax5-4.download.real-debrid.com/d/DMXTNIC44NBHC96/Se7en%20%281995%29%20%20%5B2160p%20x265%2010bit%20FS99%20Joy%5D.mkv
-wget -c "$download"
+wget -c https://lax5-4.download.real-debrid.com/d/DMXTNIC44NBHC96/Se7en%20%281995%29%20%20%5B2160p%20x265%2010bit%20FS99%20Joy%5D.mkv
+
 
 #   4K|2160|UHD 10bit|10-bit|hdr blu|bd atmos|dts-x
