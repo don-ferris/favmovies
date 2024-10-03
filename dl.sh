0e3c35b70ce0fc5b7ad7728c852aad5bd8b8af36
Don Ferris <109878120+don-ferris@users.noreply.github.com>
2024-10-02 20:35:56 -0700

    Update dl.sh

--- dl.sh
+++ dl.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget -c https://lax5-4.download.real-debrid.com/d/QJQFZR7ISIDFE89/Batman.Begins.2005.2160p.UHD.BluRay.x265.10bit.HDR.DTS-HD.MA.5.1-RARBG.mkv
+wget -c https://lax4-4.download.real-debrid.com/d/H2UIJ7ZWOQGB298/Batman.Begins.2005.2160p.UHD.BluRay.x265.10bit.HDR.DTS-HD.MA.5.1-RARBG.mkv
 
 #   4K|2160|UHD 10bit|10-bit|hdr blu|bd atmos|dts-x
