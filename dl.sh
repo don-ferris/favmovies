e4312f63de6dab623b7e5b631967024c270bca64
Don Ferris <109878120+don-ferris@users.noreply.github.com>
2024-10-02 19:25:34 -0700

    Update dl.sh

--- dl.sh
+++ dl.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget -c https://lax3-4.download.real-debrid.com/d/GTPE4VNE7RSVS40/A.River.Runs.Through.It.1992.2160p.UHD.BluRay.x265.10bit.HDR.DTS-HD.MA.5.1-RARBG.mkv
+wget -c https://lax5-4.download.real-debrid.com/d/QJQFZR7ISIDFE89/Batman.Begins.2005.2160p.UHD.BluRay.x265.10bit.HDR.DTS-HD.MA.5.1-RARBG.mkv
 
 #   4K|2160|UHD 10bit|10-bit|hdr blu|bd atmos|dts-x
