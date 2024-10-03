b0fdf6fc9fcfadae19077123b6a3590e428e96e0
Don Ferris <109878120+don-ferris@users.noreply.github.com>
2024-10-03 15:48:10 -0700

    Update dl.sh

--- dl.sh
+++ dl.sh
@@ -4,4 +4,4 @@ wget -c https://lax4-4.download.real-debrid.com/d/HQHF7H2URP7T621/As.Good.as.It.
 
 #   4K|2160|UHD 10bit|10-bit|hdr blu|bd atmos|dts-x
 
-#   git clone https://#   ghp_XOoI9fh6qyF9i5oY 3z9ikBd4hnXGsk45u9NX@github.com/don-ferris/gimme.git
+#   git clone https://ghp_XOoI9fh6qyF9i5oY 3z9ikBd4hnXGsk45u9NX@github.com/don-ferris/gimme.git
