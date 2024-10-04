a3926b334bf7da14ec25252c1acfa64dcfa985f0
Don Ferris <109878120+don-ferris@users.noreply.github.com>
2024-10-03 20:18:08 -0700

    Update dl.sh

--- dl.sh
+++ dl.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget -c https://lax4-4.download.real-debrid.com/d/HQHF7H2URP7T621/As.Good.as.It.Gets.1997.2160p.UHD.BluRay.x265.10bit.HDR.DDP5.1-RARBG.mkv
+# wget -c https://lax4-4.download.real-debrid.com/d/HQHF7H2URP7T621/As.Good.as.It.Gets.1997.2160p.UHD.BluRay.x265.10bit.HDR.DDP5.1-RARBG.mkv
 
 
 #   4K|2160|UHD 10bit|10-bit|hdr blu|bd atmos|dts-x
