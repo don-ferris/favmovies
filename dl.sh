03355d4b99e7d2bff810d028c4f0ae649136e471
Don Ferris <109878120+don-ferris@users.noreply.github.com>
2024-10-03 19:55:48 -0700

    Update dl.sh

--- dl.sh
+++ dl.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-wget -c https://lax4-4.download.real-debrid.com/d/HQHF7H2URP7T621/As.Good.as.It.Gets.1997.2160p.UHD.BluRay.x265.10bit.HDR.DDP5.1-RARBG.mkv
+# wget -c https://lax4-4.download.real-debrid.com/d/HQHF7H2URP7T621/As.Good.as.It.Gets.1997.2160p.UHD.BluRay.x265.10bit.HDR.DDP5.1-RARBG.mkv
 
 
 #   4K|2160|UHD 10bit|10-bit|hdr blu|bd atmos|dts-x
-cd ..
-git clone https://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@github.com/don-ferris/gimme.git
-
-
+export GIT_TOKEN="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"
+git clone https://don-ferris:${GIT_TOKEN}@github.com/don-ferris/gimme.git
